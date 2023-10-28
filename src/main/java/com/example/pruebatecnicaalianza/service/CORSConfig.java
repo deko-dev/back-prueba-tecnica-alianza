44afcbd822d6eb2da15dbe945946b9e75fe6b6b9
Deyser Orozco <deiser1999@gmail.com>
2023-10-28 01:23:16 -0500

    Configurando CROSS v2

---
+++ src/main/java/com/example/pruebatecnicaalianza/service/CORSConfig.java
@@ -0,0 +1,19 @@
+package com.example.pruebatecnicaalianza.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CORSConfig {
+    @Bean
+    public WebMvcConfigurer CORSConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT","DELETE");
+            }
+        };
+    }
+}
